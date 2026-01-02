96bd97ee00a44713006ba602b102dbdeb8397c1d
Christos Nounis <christosno1984@gmail.com>
2026-01-02 16:15:22 +0200

    add backend logic

---
+++ src/app/api/posts/[id]/like+api.ts
@@ -0,0 +1,45 @@
+// src/app/api/posts/[id]/like+api.ts
+
+import { getDecodedToken } from "../../../../utils/serverAuth";
+import { neon } from "@neondatabase/serverless";
+
+const sql = neon(process.env.NEON_DB_URL!);
+
+export async function POST(request: Request, { id }: { id: string }) {
+  const token = getDecodedToken(request);
+  if (!token) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const [like] = await sql`
+      INSERT INTO likes (user_id, post_id) 
+      VALUES (${token.id}, ${id})
+      ON CONFLICT (user_id, post_id) DO NOTHING 
+      RETURNING * ;`;
+
+    return new Response(JSON.stringify(like), { status: 201 });
+  } catch (error) {
+    console.error("Database Error:", error);
+    return new Response("Error liking post", { status: 500 });
+  }
+}
+
+export async function DELETE(request: Request, { id }: { id: string }) {
+  const token = getDecodedToken(request);
+
+  if (!token) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  try {
+    const [like] = await sql`
+      DELETE FROM likes 
+      WHERE user_id = ${token.id} AND post_id = ${id};`;
+
+    return new Response(JSON.stringify(like), { status: 200 });
+  } catch (error) {
+    console.error("Database Error:", error);
+    return new Response("Error unliking post", { status: 500 });
+  }
+}
