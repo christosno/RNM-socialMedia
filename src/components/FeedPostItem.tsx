59293e658d08c19c4cc364de857b8b126cf0af02
Christos Nounis <christosno1984@gmail.com>
2026-01-03 16:40:45 +0200

    fix optimistic updates

--- src/components/FeedPostItem.tsx
+++ src/components/FeedPostItem.tsx
@@ -17,37 +17,17 @@ export function FeedPostItem({ post }: FeedPostItemProps) {
   const { session } = useAuth();
   const queryClient = useQueryClient();
 
-  const {mutate: likePost} = useMutation({
+  const likePostMutation = useMutation({
     mutationFn: () => likePostRequest(post.id, session?.accessToken!),
-    onMutate: async (_, context) => {
-      await context.client.cancelQueries({ queryKey: ["posts"] });
-      const previousPosts = context.client.getQueryData(["posts"]);
-      context.client.setQueryData(["posts"], (old: any) => old.map((p: Post) => p.id === post.id ? { ...p, is_liked: true, likes_count: p.likes_count + 1 } : p));
-      return { previousPosts };
-    },
-    onError: (error, _posts , onMutateResult , context) => {
-      console.error(error);
-      context.client.setQueryData(["posts"], onMutateResult?.previousPosts);
-    },
-    onSettled: (_data, _error, _variables, _onMutateResult, context) => {
-      context.client.invalidateQueries({ queryKey: ["posts"] });
+    onSettled: () => {
+      return queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
   });
 
-  const {mutate: unlikePost} = useMutation({
+  const unlikePostMutation = useMutation({
     mutationFn: () => unlikePostRequest(post.id, session?.accessToken!),
-    onMutate: async (_, context) => {
-      await queryClient.cancelQueries({ queryKey: ["posts"] });
-      const previousPosts = queryClient.getQueryData(["posts"]);
-      queryClient.setQueryData(["posts"], (old: any) => old.map((p: Post) => p.id === post.id ? { ...p, is_liked: false, likes_count: p.likes_count - 1 } : p));
-      return { previousPosts };
-    },
-    onError: (error, _posts , onMutateResult , context) => {
-      console.error(error);
-      queryClient.setQueryData(["posts"], onMutateResult?.previousPosts);
-    },
-    onSettled: (_data, _error, _variables, _onMutateResult, context) => {
-      context.client.invalidateQueries({ queryKey: ["posts"] });
+    onSettled: () => {
+      return queryClient.invalidateQueries({ queryKey: ["posts"] });
     }
   });
 
@@ -81,15 +61,35 @@ export function FeedPostItem({ post }: FeedPostItemProps) {
             <MaterialCommunityIcons name="repeat" size={20} color="gray" />
             <Text className="text-gray-500">{post.retweets_count}</Text>
           </View>
-          <View className="flex-row items-center gap-1">
-            <MaterialCommunityIcons
-              onPress={() => post.is_liked ? unlikePost() : likePost()}
-              name={post.is_liked ? "heart" : "heart-outline"}
-              size={20}
-              color={post.is_liked ? "crimson" : "gray"}
-            />
-            <Text className="text-gray-500">{post.likes_count}</Text>
-          </View>
+          {likePostMutation.isPending ? (
+              <View className="flex-row items-center gap-1">
+              <MaterialCommunityIcons
+                name={"heart"}
+                size={20}
+                color={ "crimson" }
+              />
+              <Text className="text-gray-500">{post.likes_count + 1}</Text>
+            </View>
+            ) : unlikePostMutation.isPending ? (
+              <View className="flex-row items-center gap-1">
+              <MaterialCommunityIcons
+                name={"heart-outline"}
+                size={20}
+                color={ "grey" }
+              />
+              <Text className="text-gray-500">{post.likes_count -1 }</Text>
+            </View>
+            ) : (
+             <View className="flex-row items-center gap-1">
+              <MaterialCommunityIcons
+                onPress={() => post.is_liked ? unlikePostMutation.mutate() : likePostMutation.mutate()}
+                name={post.is_liked ? "heart" : "heart-outline"}
+                size={20}
+                color={post.is_liked ? "crimson" : "gray"}
+              />
+              <Text className="text-gray-500">{post.likes_count}</Text>
+            </View>
+          )}
         </View>
       </View>
     </View>
